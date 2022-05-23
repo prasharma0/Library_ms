68b97aaff1698960151aec842fa5fc9451333092
Prashant Sharma <prashant.sharma.ps725@gmail.com>
2022-05-23 10:23:11 +0545

    removing the config

--- config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  return mongoose.connect("mongodb://localhost:27017/lmsys")
-  .then(() => console.log(`connection to database established...`))
-  .catch((err)=> console.log(err));
-};
-module.exports = connectDB; 
